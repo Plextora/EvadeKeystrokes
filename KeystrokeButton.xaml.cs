b5ef9a4c76531ffe6c24fe905c43ce3d7c4cdb44
Plextora <plextora@pm.me>
2022-11-08 19:50:44 -0500

    Added keystroke button user control

---
+++ KeystrokeButton.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ButtonEx
+{
+    /// <summary>
+    /// Standard button with extensions
+    /// </summary>
+    public partial class KeystrokeButton : Button
+    {
+        readonly static Brush DefaultPressedBackgroundValue = new BrushConverter().ConvertFromString("#FFBEE6FD") as Brush;
+
+        public KeystrokeButton()
+        {
+            InitializeComponent();
+        }
+
+        public Brush PressedBackground
+        {
+            get { return (Brush)GetValue(PressedBackgroundProperty); }
+            set { SetValue(PressedBackgroundProperty, value); }
+        }
+        public static readonly DependencyProperty PressedBackgroundProperty = DependencyProperty.Register(
+            "PressedBackground", typeof(Brush), typeof(KeystrokeButton), new PropertyMetadata(DefaultPressedBackgroundValue));
+    }
+}

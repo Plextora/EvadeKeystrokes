17b986f33fd8cffb2ab0f0fae545c48723c870c8
Plextora <plextora@pm.me>
2022-11-10 06:15:20 -0500

    Added some goofy functionality to W keystroke

--- EvadeKeystrokes.xaml.cs
+++ EvadeKeystrokes.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Windows;
-
+using System.Windows.Forms;
+using System.Windows.Media;
 using Gma.System.MouseKeyHook;
 using KeyEventArgs = System.Windows.Forms.KeyEventArgs;
+using Button = System.Windows.Controls.Button;
 
 namespace EvadeKeystrokes
 {
@@ -11,6 +13,8 @@ namespace EvadeKeystrokes
     public partial class MainWindow : Window
     {
         private IKeyboardMouseEvents m_GlobalHook;
+        private static readonly Brush DefaultPressedBackgroundValue = new BrushConverter().ConvertFromString("#FFBEE6FD") as Brush;
+        private static readonly Brush DefaultBackgroundValue = new BrushConverter().ConvertFromString("#FFDDDDDD") as Brush;
 
         public MainWindow()
         {
@@ -23,16 +27,42 @@ namespace EvadeKeystrokes
             Unsubscribe();
         }
 
+        private void ActivateButton(Button buttonName)
+        {
+            buttonName.Background = DefaultPressedBackgroundValue;
+        }
+
+        private void DeactivateButton(Button buttonName)
+        {
+            buttonName.Background = DefaultBackgroundValue;
+        }
+
         public void Subscribe()
         {
             m_GlobalHook = Hook.GlobalEvents();
 
             m_GlobalHook.KeyDown += OnKeyDown;
+            m_GlobalHook.KeyUp += OnKeyUp;
         }
 
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
-            // do cool stuff
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    ActivateButton(WKeystroke);
+                    break;
+            }
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    DeactivateButton(WKeystroke);
+                    break;
+            }
         }
 
         public void Unsubscribe()
